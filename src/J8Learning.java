3ffc217036b821b873847981b48d838160820afc
suhshaik <suhail.shaikh@sapient.com>
2018-11-14 10:08:56 +0000

    First commit

---
+++ src/J8Learning.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.OptionalDouble;
+import java.util.stream.Stream;
+
+public class J8Learning {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        Stream<Integer> intStream = Stream.of(1, 2, 3, 4, 5,6);
+
+
+
+    }
+}
